1d8d704db04ae9d446e67e4e67538e0c1a92301b
evgen <EvgeniiGill>
2019-01-21 11:42:36 +0300

    un

--- src/main/java/com/epam/brest/cources/DelivaryCost.java
+++ src/main/java/com/epam/brest/cources/DelivaryCost.java
@@ -1,20 +1,24 @@
 
 package com.epam.brest.cources;
 
+import com.epam.brest.cources.Impl.PropertiesGetPriceImpl;
+import com.epam.brest.cources.exeption.PropertyExeption;
+import com.epam.brest.cources.intf.PropertiesGetPrice;
+import com.epam.brest.cources.reader.PriceProperty;
+
 import java.io.*;
-import com.epam.brest.cources.impl.PropertiesGetPriceImpl;
 
 public class DelivaryCost {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws PropertyExeption {
 
-        try {
-            final PropertiesGetPrice price = new PropertiesGetPriceImpl();
-            final PriceProperty priceProperty = price.getPropertiesPrice();
-            System.out.println("priceProperty = " + priceProperty);
-        }catch (final IOException ex){
-            ex.getMessage();
-        }
-        }
+       try {
+           final PropertiesGetPrice price = new PropertiesGetPriceImpl();
+           final PriceProperty priceProperty = price.getPropertiesPrice();
+           System.out.println("priceProperty = " + priceProperty);
+       }catch (final IOException ex){
+           ex.getMessage();
+       }
     }
+}
 
 
--- src/main/java/com/epam/brest/cources/PropertiesGetPrice.java
+++
@@ -1,9 +0,0 @@
-package com.epam.brest.cources;
-
-import java.io.FileNotFoundException;
-
-public interface PropertiesGetPrice {
-
-    PriceProperty getPropertiesPrice() throws FileNotFoundException;
-}
-
